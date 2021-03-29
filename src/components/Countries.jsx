d6b8f7fe8c7e9ac02dcba1bee695fe392819e82b
cleverGek <kg.geku@gmail.com>
2021-03-29 16:04:26 +0100

    l'ajout du Hooks

---
+++ src/components/Countries.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from 'react';
+
+const Countries = () => {
+    const [date, setDate] = useState([])
+
+
+    return (
+        <div>
+            Countries
+        </div>
+    );
+};
+
+export default Countries;
