101a25aa17e76c3652c5e683f0757727041ab411
cleverGek <kg.geku@gmail.com>
2021-03-30 12:15:25 +0100

    trie des données

--- src/components/Countries.jsx
+++ src/components/Countries.jsx
@@ -3,17 +3,36 @@ import axios from "axios";
 import Card from './Card';
 
 const Countries = () => {
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [sortedData, setSortedData] = useState([]);
+    const [playOne, setPlayOne] = useState(true);
+    const [rangeValue, setRangeValue] = useState(40);
 
     useEffect(() => {
-        axios.get('https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag').then((res) => setData(res.data));
-        console.log(data);
-    }, [data]);
+        if(playOne){
+            axios.get("https://restcountries.eu/rest/v2/all?fields=name;population;region;capital;flag")
+            .then((res) => {
+                setData(res.data);
+                setPlayOne(false);
+            });
+        }
+
+
+        const sortedCountry = () => {
+            const countryObj = Object.keys(data).map((i) => data[i]);
+            const sortedArray = countryObj.sort((a, b) => {
+                return b.population - a.population;
+            });
+            sortedArray.length = rangeValue;
+            setSortedData(sortedArray)
+        }
+        sortedCountry();
+    }, [data, playOne, rangeValue]);
 
     return (
         <div className="countries">
             <ul className="countries-list">
-                {data.map((country) => (
+                {sortedData.map((country) => (
                     <Card country={country} key={country.name} />
                 ))}
             </ul>
